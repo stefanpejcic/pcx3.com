5ddf9d8d9b09c8b0c8c107fb1a059cac839d1802
Stefan Pejcic <stefan@pejcic.rs>
2022-08-26 11:19:57 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
                 </a>
               </Link>{' '}
               about my journey as an Entrepreneur. */}
-              Have a good read! <span className="waving-hand text-2xl">👋🏻</span>
+              Save your time and money with ready-to-use solutions and grow your business faster! <span className="waving-hand text-2xl">⚡</span>
             </p>
             <p className="leading-7 text-gray-500 underline underline-offset-4 sm:pr-6 sm:text-lg">
               <Link
