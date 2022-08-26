2624eeb392f58bce6be359873f70977441eb1812
Stefan Pejcic <stefan@pejcic.rs>
2022-08-26 11:11:05 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
               club
             </h1>
             <p className="text-black dark:text-white sm:pr-6 sm:text-lg sm:leading-8">
-              Our WordPress plugins help 30,000+ website owners to increase <span className="">sales</span>, engage visitors, collect leads, and more.
+              Our WordPress plugins help 30,000+ website owners to increase sales, engage visitors, collect leads, and more.
               training and <span className="">Full-Stack Developer</span> by passion who loves
               building for the web & mobile. Also sometimes for the toaster.{' '}
               {/* In my
