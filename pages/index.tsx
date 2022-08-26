1d4fa61b028de268e4d39bfb0ec92a9052f5eaa1
Stefan Pejcic <stefan@pejcic.rs>
2022-08-26 10:57:18 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,9 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
               club
             </h1>
             <p className="text-black dark:text-white sm:pr-6 sm:text-lg sm:leading-8">
-              Our WordPress plugins help 30,000+ website owners to increase sales, engage visitors, collect leads, and more.{' '}
+               Welcome to my Digital Garden – I am a <span className="">Data Scientist</span> by
+              training and <span className="">Full-Stack Developer</span> by passion who loves
+              building for the web & mobile. Also sometimes for the toaster.{' '}
               {/* In my
               free time, I like developing{' '}
               <Link
